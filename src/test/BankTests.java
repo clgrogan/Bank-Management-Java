a61bf91c8f774509a92f066e398fedfdd8f08fad
Curt Grogan <curt.grogan@fdmgroup.com>
2022-08-18 14:40:35 -0400

    Transaction setup clones created

---
+++ src/test/BankTests.java
@@ -0,0 +1,14 @@
+package src.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.Test;
+
+public class BankTests {
+
+  @Test
+  public void checkingCloneTest() {
+    assertNotEquals(expected, actual);
+  }
+}
